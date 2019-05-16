0d64bcce229562d523e92eaec00c4864cbb7c297
lsylusiyao <lsylusiyao@qq.com>
2019-05-16 17:42:11 +0800

    加入原始图像

--- ShowDirSpeed.m
+++ ShowDirSpeed.m
@@ -1,5 +1,8 @@
 function result = ShowDirSpeed(path)
 % 将VR手柄数据解析为0123之类的函数
+
+    %% 数据提取部分
+
     data = load(path);
     divisionPoint = [15,25,40,55];
     dataColor = data(:,4);
@@ -7,7 +10,10 @@ function result = ShowDirSpeed(path)
     dataDirection = ones(length(dataSpeed),1);
     dataLR = data(:,7);
     dataFB = data(:,8);
-    
+    t = (1:length(data)) .* 25;
+        
+    %% 数据转换部分
+
     dataColor(dataColor >= divisionPoint(1)) = -1;
     dataColor(dataColor <= -divisionPoint(1)) = 1;
     dataColor(dataColor < divisionPoint(1) & dataColor > -divisionPoint(1)) = 0;
@@ -24,24 +30,58 @@ function result = ShowDirSpeed(path)
     dataDirection(dataDirection == 2 & dataLR > 0) = 3;
     dataDirection(dataDirection == 1 & dataFB <= 0) = 0;
     % 剩下的都是前进了
+
+    % 把direction里面是0的部分对应的dataSpeed变成0，因为没有意义
+    dataSpeed(dataDirection == 0) = 0;
     
+    %% 过滤前的图像部分
     figure;
-    plot(dataColor);
-    xlabel('时间');
+    plot(t, data(:,4)); %dataColor
+    xlabel('时间 / ms');
+    ylabel('角度');
+    title('颜色通道原始数据');
+    grid on;
+
+    subplot(1,3,1);
+    plot(t, data(:,6)); %dataSpeed
+    xlabel('时间 / ms');
+    ylabel('角度');
+    title('速度通道原始数据');
+    grid on;
+
+    subplot(1,3,2);
+    plot(t, data(:,7)); %dataLR
+    xlabel('时间 / ms');
+    ylabel('横坐标');
+    title('触摸盘的横坐标原始数据');
+    grid on;
+
+    subplot(1,3,3);
+    plot(t, data(:,8)); %dataFB
+    xlabel('时间 / ms');
+    ylabel('纵坐标');
+    title('触摸盘纵坐标原始数据');
+    grid on;
+
+    %% 过滤后的图像部分
+
+    figure;
+    plot(t, dataColor);
+    xlabel('时间 / ms');
     ylabel('颜色切换方向');
     title('颜色切换图像');
     grid on;
     
-    figure;
-    plot(dataSpeed);
-    xlabel('速度');
+    subplot(1,2,1);
+    plot(t, dataSpeed);
+    xlabel('时间 / ms');
     ylabel('速度档位');
     title('速度切换图像');
     grid on;
     
-    figure;
-    plot(dataDirection);
-    xlabel('方向');
+    subplot(1,2,2);
+    plot(t, dataDirection);
+    xlabel('时间 / ms');
     ylabel('方向切换档位');
     title('方向切换图像');
     grid on;
